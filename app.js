b89fb42ab6ba3018ce9c3bf0e21153dd37441e6b
tiagocorreia <tiagoalmeida.correia@gmail.com>
2017-11-12 21:23:21 +0000

    Passport setup

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const hbs = require("hbs");
 const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 const passportConfig = require("./controllers/passport");
+const mongoose = require("./config/mongoose");
+const user = require("./routes/user");
 
+passportConfig(passport);
 
 const app = express();
 
@@ -14,21 +20,53 @@ app.set("view engine","hbs");
 
 hbs.registerPartials(__dirname+"/views/partials");
 
+app.use(session({
+    secret:process.env.SECRET,
+    resave:false,
+    saveUninitialized:false,
+    store: new MongoStore({
+        mongooseConnection:mongoose.connection
+    })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use((req,res,next)=>{
+    res.locals.isAuthenticated = req.isAuthenticated();
+    next();
+})
+
+
+app.get("/auth/facebook",passport.authenticate("facebook"));
+app.get("/auth/facebook/callback",passport.authenticate("facebook",{failureRedirect:"/"}),(req,res)=>{
+    res.redirect("/");
+});
+
+app.get("/auth/github",passport.authenticate("github"));
+app.get("/auth/github/callback",passport.authenticate("github",{failureRedirect:"/"}),(req,res)=>{
+    res.redirect("/");
+});
+
+app.get("/auth/twitter",passport.authenticate("twitter"));
+app.get("/auth/twitter/callback",passport.authenticate("twitter",{failureRedirect:"/"}),(req,res)=>{
+    res.redirect("/");
+});
+
 app.get("/",(req,res)=>{
     res.render("index");
 });
 
-app.get("/profile",(req,res)=>{
-    res.render("user");
+app.use("/user",user);
+
+app.get("/books",(req,res)=>{
+    res.render("books");
 });
 
-app.get("/requests",(req,res)=>{
-    res.render("requests");
-});
-
-app.get("/mybooks",(req,res)=>{
-    res.render("mybooks");
-});
+app.get("/logout",(req,res)=>{
+    req.logOut();
+    req.session.destroy();
+    res.redirect("/");
+})
 
 app.use((req,res)=>{
     res.render("404");
--- controllers/passport.js
+++ controllers/passport.js
@@ -2,3 +2,75 @@ const FacebookStrategy = require("passport-facebook");
 const TwitterStrategy = require("passport-twitter");
 const GithubStrategy = require("passport-github");
 const moment = require("moment");
+
+const User = require("../models/user");
+
+module.exports = (passport) => {
+    passport.use(new FacebookStrategy({
+        clientID: process.env.FB_ID,
+        clientSecret: process.env.FB_SECRET,
+        callbackURL: "http://localhost:3000/auth/facebook/callback",
+        profileFields: ["id", "displayName", "photos"]
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({ oauthID: profile.id, socialnetwork: "facebook" }).then((user) => {
+            if (user) return user;
+            return new User({
+                oauthID: profile.id.toString(),
+                username: profile.displayName,
+                socialnetwork: "facebook",
+                photo: profile.photos[0].value,
+                createdAt: moment().valueOf()
+            }).save()
+        }).then(user => done(null, user))
+            .catch(e => done(e));
+    }));
+
+    passport.use(new GithubStrategy({
+        clientID: process.env.GIT_ID,
+        clientSecret: process.env.GIT_SECRET,
+        callbackURL: "http://localhost:3000/auth/github/callback"
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({ oauthID: profile.id, socialnetwork: "github" }).then((user) => {
+            if (user) return user;
+            return new User({
+                oauthID: profile.id.toString(),
+                username: profile.username,
+                socialnetwork: "github",
+                photo: profile.photos[0].value,
+                createdAt: moment().valueOf()
+            }).save()
+        }).then(user => done(null, user))
+            .catch(e => done(e));
+    }));
+
+        
+    passport.use(new TwitterStrategy({
+        consumerKey: process.env.TWITTER_ID,
+        consumerSecret: process.env.TWITTER_SECRET,
+        callbackURL:"http://127.0.0.1:3000/auth/twitter/callback"
+    },(accessToken ,refreshToken, profile, done)=>{
+        User.findOne({oauthID:profile.id}).then((user)=>{
+            if(user) return user;
+             return new User({
+                oauthID:profile.id.toString(),
+                username:profile.username,
+                socialnetwork: "twitter",
+                photo:profile.photos[0].value,
+                createdAt:moment().format("Do MMMM YYYY")
+            }).save()
+        }).then((user)=>done(null,user))
+        .catch((e)=>done(e));
+    }
+    ));
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id).then(data => done(null, data))
+            .catch((e) => {
+                console.log(e);
+            });
+    });
+}
